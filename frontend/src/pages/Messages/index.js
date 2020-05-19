5c010dad0a27c38cb3c46d76825caae1b962975b
lcsoares10 <lucassoares81@hotmail.com>
2020-05-18 21:47:04 -0300

    Configurando Estrutura frontEnd

---
+++ frontend/src/pages/Messages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//------------------------------------------------------------
+
+export default function Messages() {
+
+
+    return (
+        <div className="profile-container">
+            <h1>Messages</h1>
+        </div>
+    );
+}
---
+++ frontend/src/pages/RegisterContract/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//------------------------------------------------------------
+
+export default function RegisterContract() {
+
+
+    return (
+        <div className="profile-container">
+            <h1>Register Contract</h1>
+        </div>
+    );
+}
