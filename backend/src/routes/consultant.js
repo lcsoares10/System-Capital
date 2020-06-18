e713440f28285ef8826c538dc4bf8194ca3a7e43
igormotta92 <igor.mottta@gmail.com>
2020-06-18 19:43:11 -0300

    Criando rotas de rendimentos do consultor, ano e mês

--- backend/src/routes/consultant.js
+++ backend/src/routes/consultant.js
@@ -7,10 +7,14 @@ const authAdmin = require('@/src/middleware/authAdmin');
 const ConsultantValidUser = require('@/src/middleware/ConsultantValidUser');
 
 const ConsultantController = require('@/src/controllers/ConsultantController');
+const ConsultantYeldController = require('@/src/controllers/ConsultantYeldController');
 
 routes.get('/', authAdmin, ConsultantController.index);
 routes.get('/:id', ConsultantValidUser, ConsultantController.get);
 routes.get('/:id/investors', ConsultantController.getInvestors);
+
+routes.get('/:id/yield/:year', ConsultantYeldController.getYield);
+routes.get('/:id/yield/:year/month-detail/:month', ConsultantYeldController.getYieldMonthDetail);
 
 routes.post('/', authAdmin, multer(multerConfig).single("profile"), ConsultantController.create);
 routes.put('/:id', ConsultantValidUser, multer(multerConfig).single("profile"), ConsultantController.update);
