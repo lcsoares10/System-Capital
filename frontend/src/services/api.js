b12217517fdea5a2305e465230317845a1fbe07b
lcsoares10 <lucassoares81@hotmail.com>
2020-06-19 10:49:46 -0300

    Adicionando eslint e prettier

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,9 +4,9 @@ import axios from 'axios';
 
 //defino qual será a base para as requições
 const api = axios.create({
-    //baseURL:'http://localhost:3333'
-    //baseURL:'http://192.168.1.2:3333'
-    baseURL:process.env.REACT_APP_BASE_URL
+  baseURL: 'http://localhost:3333',
+  //baseURL:'http://192.168.1.2:3333'
+  //baseURL:process.env.REACT_APP_BASE_URL
 });
 
 // //===================
