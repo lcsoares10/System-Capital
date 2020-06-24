d49bddbdc33dfe29946a43a778678185b38882b8
Lucas Soares <lsoares@funcionalcorp.com.br>
2020-06-23 22:11:05 -0300

    tela de de detalhe do investidor associado ao consultor terminada

--- frontend/src/controller/Consultant/index.js
+++ frontend/src/controller/Consultant/index.js
@@ -1,19 +1,25 @@
 import api from '../../services/api';
 
- async function AllAssoatedinvestors(id_consultant) {
-    try {
-
-        const { data } = await api.get(`/consultants/${id_consultant}/investors`);
-        console.log(data)
-        return data;
-
-    } catch (error) {
-  
-        console.log(error.response);
-        return error;
-    }
-
+async function AllAssoatedinvestors(id_consultant) {
+  try {
+    const { data } = await api.get(`/consultants/${id_consultant}/investors`);
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error.response);
+    return error;
+  }
 }
 
+async function getInvestorAssociated(id_investor) {
+  try {
+    const { data } = await api.get(`/consultants/${id_investor}/investors/`);
+    console.log(data);
+    return data.rows[0];
+  } catch (error) {
+    console.log(error.response);
+    return error;
+  }
+}
 
-export{AllAssoatedinvestors}
+export { AllAssoatedinvestors, getInvestorAssociated };
--- frontend/src/pages/Consultant/DetailInvestor/index.js
+++ frontend/src/pages/Consultant/DetailInvestor/index.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import moment from 'moment';
 
 import Container from '../../../components/Container';
@@ -7,55 +7,79 @@ import FooterBackground from '../../../components/FooterBackground';
 
 import convertCoinBr from '../../../utils/convertCoinBr';
 
+import { getInvestorAssociated } from '../../../controller/Consultant';
+import { formatTel } from '../../../controller/formatsStrings';
+import allContracts from '../../../controller/Investor/allContracts';
+
 import './styles.css';
 //------------------------------------------------------------
 
 export default function DetailInvestment(props) {
-
-  const [investors,setInvestors] = useState([]);
-  const [totInvestors,setTotInvestors] = useState(0);
+  const [investor, setInvestor] = useState([]);
+  const [contractsInvestor, setContractsInvestor] = useState([]);
 
   useEffect(() => {
+    async function requestGetInvestorAssciated() {
+      const dataInvestor = await getInvestorAssociated(props.match.params.id);
+      const contractsInvestor = await allContracts(props.match.params.id);
+      setInvestor(dataInvestor.user);
+      setContractsInvestor(contractsInvestor);
+    }
+    setTimeout(() => {
+      requestGetInvestorAssciated();
+    }, 500);
+  }, []);
 
+  let tel = investor.tel;
+  tel = parseInt(tel);
 
+  return (
+    <Container>
+      <HeaderBackground notLogin={true} />
+      <main className="main-associated-investors">
+        <div className="title-header">
+          <h1 className="h1-">Investidor</h1>
+          <p> &nbsp;{investor.name}</p>
+        </div>
+        <div className="content-detail-associated">
+          <div className="detail-investor">
+            <p className="weight-thin">
+              Telefone:{' '}
+              <b className="text-white">{formatTel(tel.toString())}</b>
+            </p>
+            <p style={{ marginTop: '10px' }} className="weight-thin">
+              E-mail: <b className="text-white">{investor.email}</b>
+            </p>
+          </div>
 
-    }, []);
-    
-
-    return (
-        <Container>    
-          <HeaderBackground notLogin={true}/>
-          <main className="main-associated-investors">
-            <div className="title-header">
-              <h1 className="h1-">Investidor</h1>
-              <p> &nbsp;Lucas Soares</p>
-            </div>
-            <div className="content-detail-associated">
-              <div className="detail-investor">
-                <p className="weight-thin">Telefone: <b className='text-white'>asd</b></p>
-                <p className="weight-thin">E-mail: <b className='text-white'>as</b></p>
-              </div>
-
-              <div className="content-contracts">
-                <h2>Contratos</h2>
-                <div className="list-contracts">
-                  <div className="contracts">
-                    <p>Cod:001</p>
-                    <p>Valor Investido: <b style={{color :"green"}}>10.000,00</b></p>
-                    <div className="time-contract">
-                      <p>Inicio: 01/20</p>
-                      <p>Fim: 01/21</p>
-                    </div>
+          <div className="content-contracts">
+            <h2>Contratos</h2>
+            <div className="list-contracts">
+              {contractsInvestor.map((contract) => (
+                <div className="contracts">
+                  <p>Cod: {contract.id.toString().padStart('5', '0')}</p>
+                  <p>
+                    Valor Investido:{' '}
+                    <b style={{ color: 'green' }}>
+                      {convertCoinBr(contract.value)}
+                    </b>
+                  </p>
+                  <div className="time-contract">
+                    <p>Inicio: {moment(contract.begin).format('L')}</p>
+                    <p>
+                      Fim:{' '}
+                      {moment(contract.begin)
+                        .add(contract.time, 'months')
+                        .format('L')}
+                    </p>
                   </div>
                 </div>
-              </div>
-
+              ))}
             </div>
-         
-
-          </main>
-        <FooterBackground notLogin={true}/>
-        
-      </Container>
-    );
+          </div>
+        </div>
+      </main>
+      <FooterBackground notLogin={true} />
+    </Container>
+  );
 }
